e43ab7c1b2325e43643f9da9d7c975e6824524ab
hoanglechau <hoangchau612@gmail.com>
2022-07-04 15:01:37 +0700

    Create api.js

---
+++ src/utils/api.js
@@ -0,0 +1,77 @@
+class Api {
+    constructor({ baseUrl, headers }) {
+        this._baseUrl = baseUrl;
+        this._headers = headers;
+    }
+
+    getCardList() {
+        return fetch(`${this._baseUrl}/cards`, {
+            headers: this._headers,
+        }).then(this._checkResponse);
+    }
+
+    getUserInfo() {
+        return fetch(`${this._baseUrl}/users/me`, {
+            headers: this._headers,
+        }).then(this._checkResponse);
+    }
+
+    getInitialCards() {
+        return Promise.all([this.getCardList(), this.getUserInfo()]);
+    }
+
+    addCard({ name, link }) {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify({ name, link }),
+        }).then(this._checkResponse);
+    }
+
+    removeCard(cardID) {
+        return fetch(`${this._baseUrl}/cards/${cardID}`, {
+            method: 'DELETE',
+            headers: this._headers,
+        }).then(this._checkResponse);
+    }
+
+    changeLikeStatus(cardID, like) {
+        return fetch(`${this._baseUrl}/cards/likes/${cardID}`, {
+            method: like ? 'PUT' : 'DELETE',
+            headers: this._headers,
+        }).then(this._checkResponse);
+    }
+
+    setUserInfo({ name, about }) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({ name, about }),
+        }).then(this._checkResponse);
+    }
+
+    setUserAvatar({ avatar }) {
+        return fetch(`${this._baseUrl}/users/me/avatar`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({ avatar }),
+        }).then(this._checkResponse);
+    }
+
+    _checkResponse(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Error ${res.status}`);
+    }
+}
+
+const api = new Api({
+    baseUrl: 'https://around.nomoreparties.co/v1/group-12',
+    headers: {
+        authorization: '1789319c-7844-4c96-ac53-65a440060c1a',
+        'Content-Type': 'application/json',
+    },
+});
+
+export default api;
