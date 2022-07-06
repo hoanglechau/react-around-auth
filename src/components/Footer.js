e1cffd24d2c0c18da2ab52853c6883f2415b8c16
hoanglechau <hoangchau612@gmail.com>
2022-07-06 15:48:48 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
 export default function Footer() {
-  return (
-    <footer className="footer page__section">
-      <p className="footer__copyright">
-        ©
-        {' '}
-        {new Date().getFullYear()}
-        {' '}
-        Around The U.S.
-      </p>
-    </footer>
-  );
+    return (
+        <footer className='footer page__section'>
+            <p className='footer__copyright'>
+                © {new Date().getFullYear()} Around The U.S.
+            </p>
+        </footer>
+    );
 }
