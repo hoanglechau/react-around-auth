faf39c95c19084df8817ea6ec987fb212a3d4ecf
hoanglechau <hoangchau612@gmail.com>
2022-07-06 15:25:55 +0700

    Create ProtectedRoute.js

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function ProtectedRoute({ children, loggedIn, ...props }) {
+  return <Route {...props}>{loggedIn ? children : <Redirect to="/signin" />}</Route>;
+}
