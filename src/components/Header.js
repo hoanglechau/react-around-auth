181fbdc7aca5327089a04bdfacc899a488ef6b26
hoanglechau <hoangchau612@gmail.com>
2022-07-06 15:01:59 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import logoPath from '../images/logo.svg';
+import { Route, Link } from 'react-router-dom';
 
-export default function Header() {
+export default function Header({ onSignOut, email }) {
+    function handleSignOut() {
+        onSignOut();
+    }
+
     return (
         <header className='header page__section'>
             <img
@@ -9,6 +14,24 @@ export default function Header() {
                 alt='Around the U.S. Logo'
                 className='logo header__logo'
             />
+            <Route exact path='/'>
+                <div className='header__auth-wrapper'>
+                    <p className='header__user'>{email}</p>
+                    <button className='header__logout' onClick={handleSignOut}>
+                        Log out
+                    </button>
+                </div>
+            </Route>
+            <Route path='/signup'>
+                <Link className='header__auth-link' to='signin'>
+                    Login
+                </Link>
+            </Route>
+            <Route path='/signin'>
+                <Link className='header__auth-link' to='signup'>
+                    Sign up
+                </Link>
+            </Route>
         </header>
     );
 }
