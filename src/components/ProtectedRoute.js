83c2f915aedce4f229c59a3fa3ece99f5b0f19f7
hoanglechau <hoangchau612@gmail.com>
2022-07-06 23:33:57 +0700

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 export default function ProtectedRoute({ children, loggedIn, ...props }) {
     return (
-        <Routes>
-            <Route {...props}>
-                {loggedIn ? children : <Navigate to='/signin' />}
-            </Route>
-        </Routes>
+        <Route {...props}>
+            {loggedIn ? children : <Redirect to='/signin' />}
+        </Route>
     );
 }
